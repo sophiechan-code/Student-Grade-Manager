1701cbdc7bb50d857893389ba6a9262bb0f5f894
sophiechan-code <elsasophiechan@gmail.com>
2026-03-08 16:44:14 +0800

    Create counter.py

---
+++ task2-selfstudy/src/counter.py
@@ -0,0 +1,20 @@
+class Counter:
+    """A simple counter data structure."""
+    
+    def __init__(self):
+        """Initialize empty counter."""
+        self._counts = {}  
+    
+    def add(self, item):
+        """Add one to the count of an item."""
+        
+        pass
+    
+    def get_count(self, item):
+        """Return the count of an item."""
+        
+        return 0
+    
+    def __str__(self):
+        """Return string representation."""
+        return str(self._counts)
