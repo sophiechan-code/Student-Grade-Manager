0d9cf3abff626c945b2bcc5f52d01a68b59217c3
sophiechan-code <elsasophiechan@gmail.com>
2026-03-08 17:05:25 +0800

    Update student.py

--- task1-oop/src/student.py
+++ task1-oop/src/student.py
@@ -1,1 +1,10 @@
+from .person import Person
 
+class Student(Person):
+    def __init__(self, name, student_id):
+        super().__init__(name)
+        self.student_id = student_id
+        self.grades = []
+
+    def get_role(self):
+        return "Student"
